e42a424341e90314651d05a7b7a0e0990974808b
izzy <me@insrt.uk>
2025-11-02 19:08:46 +0000

    fix: regenerate emoji mapping using correct filter

--- lifecycle/generateEmojiMapping.ts
+++ lifecycle/generateEmojiMapping.ts
@@ -1,22 +1,27 @@
 import { resolve } from "jsr:@std/path";
 
 const ordering = await fetch(
-  "https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_15_0_ordering.json"
+  "https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_17_0_ordering.json"
 ).then((res) => res.json());
 
 const Mapping: Record<string, string> = {};
+
+const RE_SHORTCODE = /^:[\w-]+:$/;
 
 for (const group of Object.keys(ordering)) {
   for (const emote of ordering[group].emoji) {
     const emoji = String.fromCodePoint(...emote.base);
 
     for (const shortcode of emote.shortcodes) {
+      if (!RE_SHORTCODE.test(shortcode)) continue;
+
       Mapping[
         shortcode
           .substring(1, shortcode.length - 1)
-          .replace(/ /g, "-")
           .toLowerCase()
       ] = emoji;
+
+      break;
     }
   }
 }
@@ -24,7 +29,7 @@ for (const group of Object.keys(ordering)) {
 Deno.writeTextFile(
   resolve(
     import.meta.dirname!,
-    "packages/client/components/ui/emojiMapping.json"
+    "../packages/client/components/ui/emojiMapping.json"
   ),
   JSON.stringify(Mapping)
 );
